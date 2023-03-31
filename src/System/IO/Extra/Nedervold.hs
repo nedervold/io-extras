bf625eea40cf7feb76ba1ed6832db2f5072d213b
Eric Nedervold <nedervoldsoftware@gmail.com>
2023-03-31 11:21:47 -0700

    Documentation.

--- src/System/IO/Extra/Nedervold.hs
+++ src/System/IO/Extra/Nedervold.hs
@@ -1,6 +1,10 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
+-- | Extra functionality for writing to files.  Allows filtering the
+-- contents, possibly through an external program, and writing contents
+-- only if changed.
+--
+-- Useful for code generation: generated code can be filtered through
+-- an external formatter and timestamps change only when file contents
+-- change.
 module System.IO.Extra.Nedervold
   ( writeFileWithConfig
   , Config(..)
@@ -11,43 +15,52 @@ import Control.Monad (unless)
 import System.Directory (doesFileExist)
 import System.IO (readFile')
 
+-- | Configuration of the writing.
 data Config =
   Config
-    { fallibleFilter :: Bool
-    -- ^ If the filter throws an 'IOError', pass the unfiltered contents instead.
-    , filterContents :: Maybe (String -> IO String)
-    -- ^ Filter the file contents.  'System.Process.readProcess' and
+    { filterContents :: Maybe (String -> IO String)
+    -- ^ Possibly filter the file contents. To filter through an
+    -- external program, 'System.Process.readProcess' and
     -- 'System.Process.readCreateProcess' might be useful.
+    , fallibleFilter :: Bool
+    -- ^ If 'True' and the filter fails with an 'IOError', catch the
+    -- exception and pass the unfiltered contents.
     , iffChanged :: Bool
-    -- ^ Write if and only if the file contents change.
+    -- ^ Write the contents if and only if the file contents will
+    -- change.
     }
 
+-- | Filter the file contents using the configuration.
 filterFileContents :: Config -> String -> IO String
-filterFileContents Config {..} str = do
-  case filterContents of
+filterFileContents config str = do
+  case filterContents config of
     Nothing -> pure str
     Just f ->
-      if fallibleFilter
+      if fallibleFilter config
         then f str `catch` catchIO
         else f str
   where
     catchIO :: IOError -> IO String
     catchIO _ = pure str
 
+-- | Write the file [if and only
+-- if](https://en.wikipedia.org/wiki/If_and_only_if) the contents will
+-- change.
 writeFileIffChanged :: FilePath -> String -> IO ()
 writeFileIffChanged fp str = do
   exists <- doesFileExist fp
   if exists
-      -- TODO I don't necessarily /need/ to read the whole file, just
-      -- enough to ensure inequality.  But I also need to make sure
-      -- the handle is closed before moving on.  So 'readFile'' could
-      -- be overkill.  Do something better, or document why this is
-      -- the right choice.
+      -- I don't need to read the /whole/ file, just enough to ensure
+      -- inequality.  But I also need to make sure the read handle is
+      -- closed before writing to it or strange things can happen.  So
+      -- 'readFile'' may be overkill.  TODO Do something better, or
+      -- document why 'readFile'' is the right choice.
     then do
       current <- readFile' fp
       unless (current == str) $ writeFile fp str
     else writeFile fp str
 
+-- | Write the string to the file using the configuration.
 writeFileWithConfig :: Config -> FilePath -> String -> IO ()
 writeFileWithConfig config fp str = do
   filtered <- filterFileContents config str
